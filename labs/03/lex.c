b973a99705d64aa2357ba7048309731b4b8a34c4
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-02-17 13:09:49 -0600

    Start of lab03

---
+++ labs/03/lex.c
@@ -0,0 +1,21 @@
+/*
+-- Author --
+Kevin Oswaldo Cabrera Navarro - A01227157
+
+-- Code at --
+https://github.com/kevintroko/compilers-lecture/tree/master/labs/03
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *readFile(char *fileName)
+{
+    return "hello";
+}
+
+int main(int argc, char *argv[])
+{
+    readFile(argv[1]);
+    return 0;
+}
